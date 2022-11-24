133ca0996c997df8294435401836726e317f12d4
Rafał Miczek <miczek.r@gmail.com>
2022-11-24 08:22:08 +0100

    Added inventory, gathering. Improved Enemy behaviour tree.

---
+++ Assets/Scripts/Entities/KillableEntityStats.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillableEntityStats : EntityStats
+{
+    public Slider healthSlider;
+    public GameObject healthSliderUI;
+    public GameObject statusBar;
+    public GameObject damageTakenText;
+
+    public override void Awake()
+    {
+        base.Awake();
+        healthSlider.value = currentHealth / maxHealth;
+    }
+
+    public override void Regenerate(int health)
+    {
+        base.Regenerate(health);
+        healthSlider.value = currentHealth / maxHealth;
+        if (currentHealth == maxHealth)
+        {
+            healthSliderUI.SetActive(false);
+        }
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        var damageTakenGameObject = Instantiate(damageTakenText);
+        var obj = damageTakenGameObject.GetComponent<TMP_Text>();
+        obj.SetText(damage.ToString());
+        damageTakenGameObject.transform.parent = statusBar.transform;
+        damageTakenGameObject.transform.localPosition = Vector3.zero;
+        damageTakenGameObject.transform.localRotation = Quaternion.identity;
+
+        healthSliderUI.SetActive(true);
+        healthSlider.value = currentHealth * 1.0f / maxHealth;
+        if (currentHealth <= 0)
+        {
+            healthSliderUI.SetActive(false);
+        }
+    }
+}
